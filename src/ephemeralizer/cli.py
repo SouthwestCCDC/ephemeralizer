cdd634b5e2d07b87ea42cf1e63bb0eb09c2a4867
George Louthan <duplico@dupli.co>
2025-01-10 16:49:13 -0600

    Initial commit

---
+++ src/ephemeralizer/cli.py
@@ -0,0 +1,79 @@
+import click
+import minio
+import logging
+import re
+import os.path
+
+logging = logging.getLogger(__name__)
+
+def get_minio_connection(remote_url, remote_access_key, remote_secret_key):
+    logging.info(f'Connecting to remote minio endpoint at {remote_url}')
+    return minio.Minio(remote_url, access_key=remote_access_key, secret_key=remote_secret_key)
+
+def load_defaults_from_config(config_path):
+    # If the config file is not readable or doesn't exist, return an empty dictionary.
+    if not os.path.exists(config_path) or not os.access(config_path, os.R_OK):
+        logging.warning(f'Config file {config_path} is not readable or does not exist')
+        return dict()
+
+    config_schema = {
+        'remote_url': 'EPH_URL',
+        'remote_bucket': 'EPH_BUCKET',
+        'remote_access_key': 'EPH_ACCESS_KEY',
+        'remote_secret_key': 'EPH_SECRET_KEY'
+    }
+    default_arguments = dict()
+
+    with open(config_path, 'r') as config_file:
+        config = config_file.read()
+        for arg, env_var in config_schema.items():
+            match = re.search(f'{env_var}="(.+)"', config)
+            if match:
+                logging.info(f'Found {arg} in config file')
+                default_arguments[arg] = match.group(1)
+
+    return default_arguments
+
+@click.group()
+@click.pass_context
+@click.option('--context-path', type=click.Path(file_okay=True, dir_okay=False, readable=True), 
+              default="/var/run/one-context/one_env", 
+              help='Path to the OpenNebula context file to read')
+def ephemeralizer(ctx, context_path):
+    ctx.ensure_object(dict)
+    # The order of precedence for defaults is:
+    #  1. Command line arguments overrule everything
+    #  2. Environment variables overrule config file
+    #  3. Anything from the config file overrules built-in defaults
+    #  4. Built-in defaults (if any)
+
+    default_args = load_defaults_from_config(context_path)
+
+    if not ctx.default_map:
+        ctx.default_map = dict()
+    if ctx.invoked_subcommand not in ctx.default_map:
+        ctx.default_map[ctx.invoked_subcommand] = dict()
+    ctx.default_map[ctx.invoked_subcommand].update(default_args)
+
+    # Next, ctx.invoked_subcommand will be called.
+
+@ephemeralizer.command('save')
+@click.pass_context
+@click.option('--remote-url', required=True, help='URL of the remote minio endpoint')
+@click.option('--remote-bucket', required=True, help='Name of the remote bucket')
+@click.option('--remote-access-key', required=True, help='Access key for the remote minio endpoint')
+@click.option('--remote-secret-key', required=True, help='Secret key for the remote minio endpoint')
+@click.option('--local-path', required=True, type=click.Path(exists=True, file_okay=False, readable=True, dir_okay=True), help='Path to the local directory to be uploaded')
+def save(ctx, remote_url, remote_bucket, remote_access_key, remote_secret_key, local_path):
+    try:
+        client = get_minio_connection(remote_url, remote_access_key, remote_secret_key)
+    except ValueError as e:
+        # "Path in endpoint is not allowed"
+        logging.error(f'Error connecting to remote minio endpoint: {e}')
+        return
+    except Exception as e:
+        logging.error(f'Error connecting to remote minio endpoint: {e}')
+        return
+
+def ephemeralize_app():
+    ephemeralizer()
