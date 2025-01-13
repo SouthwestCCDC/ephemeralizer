69107f4f9455dfbb52e6c9e93fac2b3f9aced928
George Louthan <duplico@dupli.co>
2025-01-12 19:37:12 -0600

    Code changes to get test working

--- src/ephemeralizer/cli.py
+++ src/ephemeralizer/cli.py
@@ -3,26 +3,45 @@ import minio
 import logging
 import re
 import os.path
+from io import BytesIO
+
+# TODO: Add quiesce and unquiesce commands
+
+# TODO: set a path inside the bucket as well as the bucket name, since it seems like we're
+#       moving to using a single bucket.
 
 import truststore
 truststore.inject_into_ssl()
+
+TEST_FILE = '.ephemeralizer-test'
 
 eph_log = logging.getLogger(__name__)
 
 def get_minio_connection(remote_url, remote_access_key, remote_secret_key):
     eph_log.info(f'Connecting to remote minio endpoint at {remote_url}')
     # TODO: Trust our key, use truststore
-    return minio.Minio(remote_url, access_key=remote_access_key, secret_key=remote_secret_key)
+    try:
+        return minio.Minio(remote_url, access_key=remote_access_key, secret_key=remote_secret_key)
+    except ValueError as e:
+        # "Path in endpoint is not allowed"
+        eph_log.error(f'Error setting minio endpoint: {e}')
+        exit(1)
+    except Exception as e:
+        eph_log.error(f'Error connecting to minio endpoint: {e}')
+        exit(1)
 
 def load_defaults_from_config(config_path):
     # If the config file is not readable or doesn't exist, return an empty dictionary.
     if not os.path.exists(config_path) or not os.access(config_path, os.R_OK):
-        eph_log.warning(f'Config file {config_path} is not readable or does not exist')
+        eph_log.warning(f'Config file `{config_path}` is not readable or does not exist')
         return dict()
+
+    eph_log.debug(f'Reading default arguments from config file `{config_path}`')
 
     config_schema = {
         'remote_url': 'EPH_URL',
         'remote_bucket': 'EPH_BUCKET',
+        'remote_prefix': 'EPH_PREFIX',
         'remote_access_key': 'EPH_ACCESS_KEY',
         'remote_secret_key': 'EPH_SECRET_KEY'
     }
@@ -33,7 +52,7 @@ def load_defaults_from_config(config_path):
         for arg, env_var in config_schema.items():
             match = re.search(f'{env_var}="(.+)"', config)
             if match:
-                eph_log.debug(f'Found {arg} in config file')
+                eph_log.debug(f'Read default `{arg}` from config file')
                 default_arguments[arg] = match.group(1)
 
     return default_arguments
@@ -72,46 +91,55 @@ def ephemeralizer(ctx, context_path, log_level):
 @ephemeralizer.command('test')
 @click.option('--remote-url', required=True, help='URL of the remote minio endpoint')
 @click.option('--remote-bucket', required=True, help='Name of the remote bucket')
+@click.option('--remote-prefix', required=True, help='Name of the prefix (directory) inside the bucket to use for this service')
 @click.option('--remote-access-key', required=True, help='Access key for the remote minio endpoint')
 @click.option('--remote-secret-key', required=True, help='Secret key for the remote minio endpoint')
-def test(remote_url, remote_bucket, remote_access_key, remote_secret_key):
+def test(remote_url, remote_bucket, remote_prefix, remote_access_key, remote_secret_key):
     try:
+        # Connect, although lazily so this tells us nothing.
         client = get_minio_connection(remote_url, remote_access_key, remote_secret_key)
+        # Try to find our bucket - actually exercise the connection.
+        eph_log.debug(f'Attempting to find bucket {remote_bucket}')
         found = client.bucket_exists(remote_bucket)
         eph_log.info(f'Connection to endpoint {remote_url} successful')
         if not found:
             eph_log.error(f'Bucket {remote_bucket} does not exist')
             return
-    except ValueError as e:
-        # "Path in endpoint is not allowed"
-        eph_log.error(f'Error connecting to remote minio endpoint: {e}')
-        return
+        test_file_path = f"{remote_prefix}/{TEST_FILE}"
+        # Try to write a blank file to the bucket.
+        eph_log.debug(f'Writing a blank file to {remote_bucket}/{test_file_path}')
+        res = client.put_object(remote_bucket, test_file_path, BytesIO(b''), 0)
+        eph_log.info(f'Successfully wrote {remote_bucket}/{test_file_path}')
+
+        # TODO:
+        # Technically, our permissions model says we shouldn't be able to read the
+        #  file back. Try anyway, and WARN if we can read it.
+
+        # We don't strictly need to be able to delete this file, but try anyway.
+        eph_log.debug(f'Deleting {remote_bucket}/{test_file_path}')
+        # TODO: Test this without permissions to delete
+        res = client.remove_object(remote_bucket, test_file_path)
+        eph_log.info(f'Successfully deleted {remote_bucket}/{test_file_path}')
     except minio.error.InvalidResponseError as e:
         eph_log.error(f"Got an invalid response from the remote endpoint: is it correct and running minio?")
         return
-    except Exception as e:
-        eph_log.error(f'Error connecting to remote minio endpoint: {e}')
-        return
     
-
-
-@ephemeralizer.command('save')
-@click.pass_context
-@click.option('--remote-url', required=True, help='URL of the remote minio endpoint')
-@click.option('--remote-bucket', required=True, help='Name of the remote bucket')
-@click.option('--remote-access-key', required=True, help='Access key for the remote minio endpoint')
-@click.option('--remote-secret-key', required=True, help='Secret key for the remote minio endpoint')
-@click.option('--local-path', required=True, type=click.Path(exists=True, file_okay=False, readable=True, dir_okay=True), help='Path to the local directory to be uploaded')
-def save(ctx, remote_url, remote_bucket, remote_access_key, remote_secret_key, local_path):
-    try:
-        client = get_minio_connection(remote_url, remote_access_key, remote_secret_key)
-    except ValueError as e:
-        # "Path in endpoint is not allowed"
-        eph_log.error(f'Error connecting to remote minio endpoint: {e}')
-        return
     except Exception as e:
-        eph_log.error(f'Error connecting to remote minio endpoint: {e}')
+        eph_log.error(f'{type(e)} {e}')
+        print(dir(e))
+        print(e.with_traceback())
         return
+
+# @ephemeralizer.command('save')
+# @click.pass_context
+# @click.option('--remote-url', required=True, help='URL of the remote minio endpoint')
+# @click.option('--remote-bucket', required=True, help='Name of the remote bucket')
+# @click.option('--remote-access-key', required=True, help='Access key for the remote minio endpoint')
+# @click.option('--remote-secret-key', required=True, help='Secret key for the remote minio endpoint')
+# @click.option('--local-path', required=True, type=click.Path(exists=True, file_okay=False, readable=True, dir_okay=True), help='Path to the local directory to be uploaded')
+# def save(ctx, remote_url, remote_bucket, remote_access_key, remote_secret_key, local_path):
+#     try:
+#         client = get_minio_connection(remote_url, remote_access_key, remote_secret_key)
 
 def ephemeralize_app():
     ephemeralizer()
